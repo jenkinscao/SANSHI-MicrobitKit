47f8f5bfc0d098b72a6db7df3edd36e2c3c93564
jenkinscao <jenkinscao@gmail.com>
2026-01-24 12:28:47 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-enum MotorList {
+ enum MotorList {
     //% block="M1 前左"
     M1 = 1,
     //% block="M2 前右"
@@ -108,13 +108,13 @@ namespace motorx {
     export function mecanumMove(dir: MoveDir, speed: number): void {
         let s = speed;
         switch (dir) {
-            case MoveDir.Right:
-                setAll(s, s, s, s); break;
-            case MoveDir.Left:
-                setAll(-s, -s, -s, -s); break;
-            case MoveDir.Back:
-                setAll(-s, s, s, -s); break;
             case MoveDir.Forward:
+                setAll(s, s, s, s); break;
+            case MoveDir.Back:
+                setAll(-s, -s, -s, -s); break;
+            case MoveDir.Left:
+                setAll(-s, s, s, -s); break;
+            case MoveDir.Right:
                 setAll(s, -s, -s, s); break;
             case MoveDir.LeftFront:
                 setAll(0, s, s, 0); break;
